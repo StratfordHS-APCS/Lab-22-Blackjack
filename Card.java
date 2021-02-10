8efbac9b8a66e4104775a74b037be8f0293e9367
Dave Avis <charles.avis@springbranchisd.com>
2021-02-10 13:10:36 -0600

    Add files via upload

---
+++ Card.java
@@ -0,0 +1,28 @@
+public class Card
+{
+	public static final String[] FACES = {"ZERO","ACE","TWO","THREE","FOUR",	"FIVE","SIX","SEVEN","EIGHT","NINE","TEN","JACK","QUEEN","KING"};
+
+	private String suit;
+	private int face;
+
+  	//constructors
+
+
+	// modifiers
+
+
+  	//accessors
+
+  	public int getValue()
+  	{
+  		return face;
+  	}
+
+	public boolean equals(Object obj)
+	{
+		return false;
+	}
+
+  	//toString
+  	
+ }
---
+++ Main.java
@@ -0,0 +1,20 @@
+class Main {
+  public static void main(String[] args) {
+
+    // Uncomment to test the Card class
+    //CardTestOne.main(args);
+
+    // Uncomment to test the Deck class
+    //DeckTestOne.main(args);
+
+    // Uncomment to test the Player class
+    //PlayerTestOne.main(args);
+
+    // Uncomment to test the dealer class
+    //DealerTestOne.main(args);
+
+    // Uncomment to run the final game
+    // BlackJack b = new BlackJack();
+    // b.playGame();
+  }
+}
